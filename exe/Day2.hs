5277bcc6e02f1e0bf2e3d8fc12b56a68c44d3fd1
rmgaray <ramiro.sgaray@gmail.com>
2022-12-02 16:47:53 -0300

    complete day 2 challenges

---
+++ exe/Day2.hs
@@ -0,0 +1,81 @@
+module Main where
+
+import AOC.Utils (invertOrd, liftEither, newlineParser, spaceParser)
+import Control.Applicative (Alternative ((<|>)), optional)
+import Control.Monad ((<=<))
+import Data.Attoparsec.ByteString (Parser, choice, many1, parseOnly, sepBy1, word8)
+import Data.Attoparsec.ByteString.Char8 (decimal)
+import Data.Bifunctor (second)
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.List (sortBy)
+import Data.Word (Word8)
+import Paths_aoc22 (getDataFileName)
+
+data Shape = Paper | Scissors | Rock
+  deriving (Eq, Show, Ord, Enum)
+
+data Result = Loss | Draw | Win
+  deriving (Eq, Show, Ord)
+
+-- PARSING --
+
+a, b, c :: Word8
+[a, b, c] = [65 .. 67]
+
+x, y, z :: Word8
+[x, y, z] = [88 .. 90]
+
+parseOpponentShape :: Parser Shape
+parseOpponentShape = choice [Rock <$ word8 a, Paper <$ word8 b, Scissors <$ word8 c]
+
+parseOwnShape :: Parser Shape
+parseOwnShape = choice [Rock <$ word8 x, Paper <$ word8 y, Scissors <$ word8 z]
+
+rowParser :: Parser (Shape, Shape)
+rowParser = (,) <$> (parseOpponentShape <* spaceParser) <*> (parseOwnShape <* newlineParser)
+
+-- CALCULATING VALUE --
+
+shapeValue :: Shape -> Int
+shapeValue Rock = 1
+shapeValue Paper = 2
+shapeValue Scissors = 3
+
+shapeCompare :: Shape -> Shape -> Ordering
+-- We handle the edge cases
+shapeCompare Paper Rock = GT
+shapeCompare Rock Paper = LT
+-- We reuse the derived Ord for all other cases
+shapeCompare x y = x `compare` y
+
+rowValue :: (Shape, Shape) -> Int
+rowValue (theirShape, myShape) = shapeValue myShape + matchValue
+  where
+    matchValue =
+      case myShape `shapeCompare` theirShape of
+        GT -> 6
+        EQ -> 3
+        LT -> 0
+
+toResult :: Shape -> Result
+toResult Rock = Loss
+toResult Paper = Draw
+toResult Scissors = Win
+
+reinterpretRow :: (Shape, Shape) -> (Shape, Shape)
+reinterpretRow row = case second toResult row of
+  -- We handle the two edge cases
+  (Paper, Loss) -> (Paper, Rock)
+  (Rock, Win) -> (Rock, Paper)
+  -- We use the `Enum` instances for the general cases
+  (theirShape, Loss) -> (theirShape, pred theirShape)
+  (theirShape, Draw) -> (theirShape, theirShape)
+  (theirShape, Win) -> (theirShape, succ theirShape)
+
+main :: IO ()
+main = do
+  bs <- (BS.readFile <=< getDataFileName) "inputs/day2.txt"
+  rows <- liftEither "Could not parse input" $ parseOnly (many1 rowParser) bs
+  print $ sum $ rowValue <$> rows
+  print $ sum $ rowValue . reinterpretRow <$> rows
--- src/AOC/Utils.hs
+++ src/AOC/Utils.hs
@@ -1,4 +1,7 @@
-module AOC.Utils (liftEither, invertOrd) where
+module AOC.Utils (liftEither, invertOrd, newlineParser, spaceParser) where
+
+import Data.Attoparsec.ByteString (Parser, word8)
+import Data.Word (Word8)
 
 liftEither :: String -> Either e a -> IO a
 liftEither msg = either (error msg) pure
@@ -8,3 +11,9 @@ invertOrd x y = case compare x y of
   LT -> GT
   GT -> LT
   EQ -> EQ
+
+newlineParser :: Parser Word8
+newlineParser = word8 10
+
+spaceParser :: Parser Word8
+spaceParser = word8 32
